490429616f08e8596a673bb96d49d941c84b51b1
mukesh2305 <yogahindu1999@gmail.com>
2022-02-16 11:50:27 +0530

    fontawesome use

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import { Table, Button } from "reactstrap";
+import {
+  faImage,
+  faThumbsUp,
+  faThumbsDown,
+  faMoneyCheckAlt,
+  faSearchDollar,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+class App extends Component {
+  state = {
+    isLoading: false,
+    invoices: [
+      {
+        id: 100,
+        Vendor: "Hankok",
+        Amount: "$18,000",
+        InVoice: "INV-100",
+        Date: "12/12/2019",
+      },
+      {
+        id: 200,
+        Vendor: "Hankok",
+        Amount: "$18,000",
+        InVoice: "INV-100",
+        Date: "12/12/2019",
+      },
+      {
+        id: 300,
+        Vendor: "Hankok",
+        Amount: "$18,000",
+        InVoice: "INV-100",
+        Date: "12/12/2019",
+      },
+    ],
+  };
+  remove(id) {
+    let updatedInvoices = [...this.state.invoices].filter((invoice) => {
+      return invoice.id !== id;
+    });
+    this.setState({ invoices: updatedInvoices });
+  }
+
+  render() {
+    const isLoading = this.state.isLoading;
+    const allInvoices = this.state.invoices;
+
+    if (isLoading) return <div>Loading...</div>;
+    let invoices = allInvoices.map((invoice) => {
+      return (
+        <tr key={invoice.id}>
+          <td>{invoice.Vendor}</td>
+          <td>{invoice.Amount}</td>
+          <td>{invoice.InVoice}</td>
+          <td>{invoice.Date}</td>
+          <td>
+            <Button
+              className="btn btn-lg btn-success"
+              onClick={() => this.remove(invoice.id)}
+            >
+              <FontAwesomeIcon icon={faThumbsUp} /> OK
+            </Button>
+          </td>
+          <td>
+            <Button
+              className="btn btn-lg btn-danger"
+              onClick={() => this.remove(invoice.id)}
+            >
+              <FontAwesomeIcon icon={faThumbsDown} />
+              NOK
+            </Button>
+          </td>
+          <td>
+            <Button
+              className="btn btn-lg btn-info"
+              onClick={() => this.remove(invoice.id)}
+            >
+              <FontAwesomeIcon icon={faMoneyCheckAlt} />
+              50%
+            </Button>
+          </td>
+          <td>
+            <Button
+              className="btn btn-lg btn-warning"
+              onClick={() => this.remove(invoice.id)}
+            >
+              <FontAwesomeIcon icon={faSearchDollar} />
+              ??
+            </Button>
+          </td>
+          <td>
+            <Button
+              className="btn btn-lg btn-info"
+              onClick={() => this.remove(invoice.id)}
+            >
+              <FontAwesomeIcon icon={faImage} />
+              Image
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+    return (
+      <div className="container border border-secondary rouded center">
+        <div className="row">
+          <div className="col-12">
+            <h4>Pending invoices - The Test Company</h4>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-xs-12 center text-center">
+            <Table dark responsive striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Vendor</th>
+                  <th>Amount</th>
+                  <th>Invoice #</th>
+                  <th>Date</th>
+                  <th colSpan="4">Actions</th>
+                  <th>Image</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.invoices.length === 0 ? (
+                  <td colSpan="9">All Caught up!</td>
+                ) : (
+                  invoices
+                )}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
